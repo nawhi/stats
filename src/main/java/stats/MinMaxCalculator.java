ec9a2fce63e03af2fffc3557dab1f969dccc0e64
Nick White <nawhi3@outlook.com>
2019-02-11 11:55:22 +0000

    Add second test and correct error

--- src/main/java/stats/MinMaxCalculator.java
+++ src/main/java/stats/MinMaxCalculator.java
@@ -6,8 +6,8 @@ import static java.util.Comparator.naturalOrder;
 
 public class MinMaxCalculator {
     public MinMax minMaxFor(Set<Integer> numbers) {
-        int max = numbers.stream().min(naturalOrder()).get();
-        int min = numbers.stream().max(naturalOrder()).get();
+        int min = numbers.stream().min(naturalOrder()).get();
+        int max = numbers.stream().max(naturalOrder()).get();
         return new MinMax(min, max);
     }
 }
--- src/test/java/stats/MinMaxCalculatorShould.java
+++ src/test/java/stats/MinMaxCalculatorShould.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import static java.util.Arrays.asList;
 import static org.junit.Assert.assertEquals;
 
 public class MinMaxCalculatorShould {
@@ -16,4 +17,14 @@ public class MinMaxCalculatorShould {
         Set<Integer> numbers = new HashSet<>(Collections.singletonList(1));
         assertEquals(new MinMax(1, 1), calculator.minMaxFor(numbers));
     }
+
+    @Test
+    public void give_min_and_max_of_longer_set() {
+        MinMaxCalculator calculator = new MinMaxCalculator();
+        Set<Integer> numbers = new HashSet<>(asList(1, 2, 3, 4, 5));
+        assertEquals(new MinMax(1, 5), calculator.minMaxFor(numbers));
+    }
+
+
+
 }
